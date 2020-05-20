cf45eb39f4864c1256865f4593d7a41089f58c58
Anton Oskirko <oskirych@gmail.com>
2020-05-21 02:03:08 +0300

    Добавил полное решение задачи B из раунда 643 Div 2

--- Round_643_Div_2/B/B.py
+++ Round_643_Div_2/B/B.py
@@ -1,25 +1,55 @@
-def solve(n, arr):
-    
-    while (big_num.k < k_n) and (big_num.min > 0):
-        big_num.next_num()
-    ans = big_num.output()
+# from typing import List
+
+
+def solve(n: int, arr, mmax: int) -> int:
+    ans = 0
+    res = 0
+    i = 1
+    while i <= mmax:
+        if res == 0:
+            ans += arr[i] // i
+            res = arr[i] % i
+        else:
+            if res + arr[i] < i:
+                res += arr[i]
+            else:
+                delta = arr[i] + res - i
+                ans += (1 + (delta) // i)
+                res = delta % i
+        i += 1
     return ans
 
 def prepare_data(n, inp_str):
     tmp_str = inp_str.split(' ')
     tmp = [0]*(n+1)
+    mmax = 1
     for jj in range(n):
-        tmp[int(tmp_str[jj])] += 1
-    return tmp
+        k = int(tmp_str[jj])
+        tmp[k] += 1
+        if k > mmax:
+            mmax = k
+    return tmp, mmax
 
-fin = open('input.txt', 'r')
-fout = open('output.txt', 'w')
+# fin = open('input.txt', 'r')
+# fout = open('output.txt', 'w')
 
-t = int(fin.readline())
+# t = int(fin.readline())
+t = int(input())
 for ___ in range(t):
-    n = int(fin.readline())
-    inp_str = fin.readline()
-    array = prepare_data(n, inp_str)
-    
-fin.close()
-fout.close()
+    # n = int(fin.readline())
+    # inp_str = fin.readline()
+    n = int(input())
+    inp_str = input()
+    if n == 1:
+        print(1)
+    else:
+        array, mmax = prepare_data(n, inp_str)
+        # array = [0, 199912, 15, 5, 8, 13, 7, 13, 10, 7, 10]
+        # mmax = 10
+        groups = solve(n, array, mmax)
+        print(groups)
+
+#     fout.write(str(groups) + '\n')
+# fin.close()
+# fout.close()
+# 0, 199912, 15, 5, 8, 13, 7, 13, 10, 7, 10, 0
